de3ee5cb4bcbd2c2978b2229ac649e21f355b78a
Sergei Nikiforov <Sergei.Nikiforov.-ND@disney.com>
2025-10-12 09:16:42 -0500

    5.0

---
+++ src/main/java/ru/job4j/TriangleArea.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+public class TriangleArea {
+    public static double area(double a, double b, double c) {
+        double p = (a + b + c) / 2;
+        return  Math.sqrt(p * (p - a) * (p - b) * (p - c ));
+    }
+
+    public static void main(String[] args) {
+        double result = TriangleArea.area(3, 3, 3);
+        System.out.println("area (3, 3, 3) = " + result);
+    }
+}
