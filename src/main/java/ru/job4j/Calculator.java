aee0e75e9292830eb820e3f74fe3303a8df8b330
Sergei Nikiforov <Sergei.Nikiforov.-ND@disney.com>
2025-10-13 17:53:27 -0500

    9.3

--- src/main/java/ru/job4j/Calculator.java
+++ src/main/java/ru/job4j/Calculator.java
@@ -1,4 +1,13 @@
 package ru.job4j;
 
 public class Calculator {
+
+    public static void plus(int first, int second) {
+        int result = first + second;
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        plus(1, 1);
+    }
 }
