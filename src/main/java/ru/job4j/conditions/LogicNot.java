47ebc326ce636d7eb6d317d86ac49819d85166d7
Sergei Nikiforov <Sergei.Nikiforov.-ND@disney.com>
2025-10-17 18:51:21 -0500

    4.3.3

---
+++ src/main/java/ru/job4j/conditions/LogicNot.java
@@ -0,0 +1,29 @@
+package ru.job4j.conditions;
+
+public class LogicNot {
+
+    public static boolean isEven(int num) {
+        return num % 2 == 0;
+    }
+
+    public static boolean isPositive(int num) {
+        return num > 0;
+    }
+
+    public static boolean notEven(int num) {
+        return !isEven(num);
+    }
+
+    public static boolean isNegative(int num) {
+        return num < 0;
+    }
+
+    public static boolean notEvenAndPositive(int num) {
+        return notEven(num) && isPositive(num);
+    }
+
+    public static boolean evenOrNegative(int num) {
+        return isEven(num) || isNegative(num);
+    }
+
+}
