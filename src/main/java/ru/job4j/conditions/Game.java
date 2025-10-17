2c6cae775d5a125291390e0f49698e717a9f4b4a
Sergei Nikiforov <Sergei.Nikiforov.-ND@disney.com>
2025-10-16 19:12:24 -0500

    4.1.1

---
+++ src/main/java/ru/job4j/conditions/Game.java
@@ -0,0 +1,23 @@
+package ru.job4j.conditions;
+
+public class Game {
+    public static void menu(String name) {
+        if ("super mario".equals(name)) {
+            System.out.println("Start - super mario");
+        }
+        if ("tanks".equals(name)) {
+            System.out.println("Start - tanks");
+        }
+        if ("tetris".equals(name)) {
+            System.out.println("Start - tetris");
+        } else {
+            System.out.println("another game");
+        }
+    }
+
+    public static void main(String[] args) {
+        Game.menu("tanks");
+        Game.menu("tetris");
+        Game.menu("cards");
+    }
+}
