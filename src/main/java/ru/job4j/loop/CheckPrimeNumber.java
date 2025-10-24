91cc43034869c4c3baa5fafa1fa1083ee884a543
Sergei Nikiforov <Sergei.Nikiforov.-ND@disney.com>
2025-10-24 18:42:18 -0500

    5.5 fix

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,14 +2,15 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = true;
+        boolean prime = number > 1;
 
         if (number < 2) {
             return false;
         }
         for (int i = 2; i <= Math.sqrt(number); i++) {
             if (number % i == 0) {
-                return false;
+                prime = false;
+                break;
             }
         }
         return prime;
