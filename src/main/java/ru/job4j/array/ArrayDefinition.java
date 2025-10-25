8197b813a4beb6b1c8051880b494721362c08f9e
Sergei Nikiforov <Sergei.Nikiforov.-ND@disney.com>
2025-10-24 19:00:38 -0500

    6.0.3

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -4,7 +4,13 @@ public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
+        String[] names = new String[4];
         float[] prices = new float[40];
+
+        names[0] = "Petr";
+        names[1] = "Arsentev";
+        names[2] = "Teach";
+        names[3] = "Java";
 
         System.out.println("Размер массива ages: " + ages.length);
         System.out.println("Размер массива surnames: " + surnames.length);
