cddf89da40773fb97080e8ae5feca660192a773a
Sergei Nikiforov <Sergei.Nikiforov.-ND@disney.com>
2025-10-25 10:31:48 -0500

    6.2.2

--- src/test/java/ru/job4j/array/SwitchArrayTest.java
+++ src/test/java/ru/job4j/array/SwitchArrayTest.java
@@ -35,4 +35,34 @@ class SwitchArrayTest {
         int[] expected = {1};
         assertThat(result).containsExactly(expected);
     }
+
+    @Test
+    void whenSwap0to3() {
+        int[] input = {1, 2, 3, 4};
+        int source = 0;
+        int destination = input.length - 1;
+        int[] result = SwitchArray.swap(input, source, destination);
+        int[] expected = {4, 2, 3, 1};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    void whenSwap1to2() {
+        int[] input = {1, 2, 3, 4};
+        int source = 1;
+        int destination = 2;
+        int[] result = SwitchArray.swap(input, source, destination);
+        int[] expected = {1, 3, 2, 4};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    void whenSwap7to1() {
+        int[] input = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        int source = 7;
+        int destination = 1;
+        int[] result = SwitchArray.swap(input, source, destination);
+        int[] expected = {1, 8, 3, 4, 5, 6, 7, 2, 9, 0};
+        assertThat(result).containsExactly(expected);
+    }
 }
